1ffa1336bde784503ccf3e44af11434245f7ee6f
chrizxzayax <chmolina629@hotmail.com>
2025-09-28 14:37:06 -0700

    privatepublic add

--- lab15.cpp
+++ lab15.cpp
@@ -1,3 +1,29 @@
 // comsc-210 | lab6 | Christian Molina 
 #include <iostream>
+#include <fstream>
 
+using namespace std;
+
+class film {
+    private:
+            string title;
+            int Ryear;
+            string scwriter;
+
+    public:
+            film() : title(""), Ryear(0), scwriter("") {} // default constructor
+            film(const string& t, int y, const string& s) : title(t), Ryear(y), scwriter(s) {} // constructor with parameters
+    
+
+};
+
+int main () {
+
+    ifstream fin("films.txt");
+    if (!fin) {
+        cerr << "Error opening the file" << endl;
+        return 1;
+    }
+
+    
+}
